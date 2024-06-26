05d2153c1a1af8fb88e0e2041dd5a26a6d34dcb6
Andy-Hub79 <67456711+Andy-Hub79@users.noreply.github.com>
2024-06-26 00:39:58 -0600

    Bond, size

--- DataBase/conexion.php
+++ DataBase/conexion.php
@@ -1,12 +1,12 @@
 <?php
-// Parámetros de conexión a la base de datos
-$servername = "localhost"; // Ajusta según sea necesario
-$username = "root"; // Usuario de la base de datos
-$password = "root"; // Contraseña del usuario
-$dbname = "usuarios_retiro"; // Nombre de la base de datos
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "remodelacionusuarios2";
+$port = 3307;
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar conexión
 if ($conn->connect_error) {
@@ -17,13 +17,13 @@ if ($conn->connect_error) {
 $nombre = $_POST['nombre'];
 $apellidoPaterno = $_POST['apellidoPaterno'];
 $apellidoMaterno = $_POST['apellidoMaterno'];
-$email = $_POST['email'];
+$correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
 
 // Preparar la sentencia SQL usando sentencias preparadas
-$stmt = $conn->prepare("INSERT INTO tb_users (nombre, apellidoPaterno, apellidoMaterno, email, telefono, mensaje) VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("ssssss", $nombre, $apellidoPaterno, $apellidoMaterno, $email, $telefono, $mensaje);
+$stmt = $conn->prepare("INSERT INTO users (nombre, apellidoPaterno, apellidoMaterno, correo, telefono, mensaje) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("ssssss", $nombre, $apellidoPaterno, $apellidoMaterno, $correo, $telefono, $mensaje);
 
 // Ejecutar la sentencia
 if ($stmt->execute()) {
